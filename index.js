b949bf7571ecfaf2662a531d57c4a82d19f62df8
RohitShetty <rohitshetty@credenceanalytics.com>
2025-01-30 15:23:00 +0530

    Updated message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello, CI/CD!" });
+    res.json({ message: "CI/CD is working!" });
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
